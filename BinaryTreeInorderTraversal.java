3ce6e8ecbb3cfd56aaa520e91497b54cf8225a55
DeafinitelyAbled <135254827+DeafinitelyAbled@users.noreply.github.com>
2023-07-26 14:11:56 -0400

    Binary Tree Inorder Traversal Solution

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,33 @@
+// Please, open (copy & paste) the following link for the problem description:
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorderTraversalHelper(root, result);
+        return result;
+    }
+
+    private void inorderTraversalHelper(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        inorderTraversalHelper(node.left, result);
+        result.add(node.val);
+        inorderTraversalHelper(node.right, result);
+    }
+}
